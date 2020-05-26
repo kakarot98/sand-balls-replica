3c19caf5f4a747bb7e0c3a6a56b4881b42cdf616
hrushikesh(PC) <hrushikeshmohanty90@gmail.com>
2020-05-26 22:21:20 +0530

    first commit

---
+++ Assets/Scripts/RaycastCam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCam : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = transform.GetComponent<Camera>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0)) {
+            DeformMesh();
+        }
+    }
+
+    void DeformMesh() {
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit)) {
+            //deform
+            DeformPlane deformPlane = hit.transform.GetComponent<DeformPlane>();
+            deformPlane.DeformThisPlane(hit.point);
+        }
+    }
+}
