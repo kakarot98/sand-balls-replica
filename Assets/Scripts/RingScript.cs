2e95c16159abb8352beec26251d47ff051a694d1
hrushikesh(PC) <hrushikeshmohanty90@gmail.com>
2020-05-27 22:46:07 +0530

    updated colliders

---
+++ Assets/Scripts/RingScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RingBlock") {
+            Destroy(other.gameObject);
+        }
+    }
+}
