bb0b0f9fc1b16a2e8581353a0f2eea590e284bae
hrushikesh(PC) <hrushikeshmohanty90@gmail.com>
2020-05-27 23:35:45 +0530

    deform improved

--- Assets/Scripts/RaycastCam.cs
+++ Assets/Scripts/RaycastCam.cs
@@ -11,6 +11,8 @@ public class RaycastCam : MonoBehaviour
 
     public Transform ringPrefab = default;
 
+    Vector3 lastPos;
+
     private void Start()
     {
         cam = transform.GetComponent<Camera>();
@@ -23,16 +25,21 @@ public class RaycastCam : MonoBehaviour
     }
 
     void DeformMesh() {
-        ray = cam.ScreenPointToRay(Input.mousePosition);
+        if ((lastPos = Input.mousePosition).sqrMagnitude > 2)
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit)) {
-            
-            DeformPlane deformthePlane = hit.transform.GetComponent<DeformPlane>();
-            deformthePlane.DeformThisPlane(hit.point);
+            if (Physics.Raycast(ray, out hit))
+            {
 
-            Instantiate(ringPrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z - 0.11f), Quaternion.Euler(-90, 0, 0));
+                DeformPlane deformthePlane = hit.transform.GetComponent<DeformPlane>();
+                deformthePlane.DeformThisPlane(hit.point);
 
-            
+                Instantiate(ringPrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z - 0.11f), Quaternion.Euler(-90, 0, 0));
+
+
+            }
+            lastPos = Input.mousePosition;
         }
     }
 }
--- Assets/Scripts/RingScript.cs
+++ Assets/Scripts/RingScript.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class RingScript : MonoBehaviour
 {
+
+    [SerializeField]
+    MeshCollider[] colliders;
+
+    private void Start()
+    {
+        foreach (var item in colliders) {
+            item.enabled = true;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "RingBlock") {
