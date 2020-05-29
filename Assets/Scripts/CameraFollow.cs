02b011a1d961e47d346639e4e7cbf36ba3031c3e
hrushikesh(PC) <hrushikeshmohanty90@gmail.com>
2020-05-29 20:59:54 +0530

    pause and resume screen

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera cam;
+    float moveOffset = 2f;
+    float mouseFollowStartPos = 250f; 
+
+        
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0)) {
+            if (Input.mousePosition.y < mouseFollowStartPos) {
+                cam.transform.position = new Vector3(cam.transform.position.x, (cam.transform.position.y - moveOffset * Time.deltaTime), cam.transform.position.z);
+            }
+        }
+    }
+}
